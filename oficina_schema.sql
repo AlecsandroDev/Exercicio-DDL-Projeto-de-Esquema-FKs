b0528d2da198c8ca2500448edadec4e0f8b89487
Alecsandro_Dev <107969775+AlecsandroDev@users.noreply.github.com>
2025-10-23 15:32:05 -0300

    Update oficina_schema.sql
    Formatação.

--- oficina_schema.sql
+++ oficina_schema.sql
@@ -10,13 +10,7 @@
 	-- cliente.
 	-- endereco: não pode ser vazio, pois serve para encontrar a cas do cliente,
 	-- em caso de deixar o veículo na residência.
-CREATE TABLE cliente (
-    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf CHAR(11) UNIQUE NOT NULL,
-    numero_telefone VARCHAR(20) NOT NULL,
-    endereco CHAR(100) NOT NULL
-);
+
 
 -- Tabela de Veículos:
 	-- id_veiculo: vai ser a PK, para identificar cada veículo na tabela, sendo
@@ -34,16 +28,7 @@ CREATE TABLE cliente (
 	-- ano_fabricacao: armazena o ano de fabricação do veículo, sendo obrigatório,
     -- pois veículos podem ser diferente devido o ano de fabricação, mesmo sendo o
     -- mesmo modelo.
-CREATE TABLE veiculo (
-    id_veiculo INT AUTO_INCREMENT PRIMARY KEY,
-    id_cliente_ref INT NOT NULL,
-    placa CHAR(7) UNIQUE NOT NULL,
-    renavam VARCHAR(11) UNIQUE NOT NULL,
-    marca VARCHAR(40) NOT NULL,
-    modelo VARCHAR(60) NOT NULL,
-    cor VARCHAR(30) NOT NULL,
-    ano_fabricacao DATE NOT NULL
-);
+
 
 -- Tabela de Serviços:
 	-- id_servico: vai ser a PK, usada para identificar cada tipo de serviço oferecido.
@@ -52,13 +37,6 @@ CREATE TABLE veiculo (
 	-- duracao: indica o tempo médio estimado de execução do serviço.
 	-- preco: deve ser maior que zero, pois representa o valor cobrado pelo serviço,
     -- e não pode ser um valor vazio.
-CREATE TABLE servico (
-    id_servico INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(80) UNIQUE NOT NULL,
-    descricao VARCHAR(255),
-    duracao TIME,
-    preco DOUBLE NOT NULL CHECK (preco > 0)
-);
 
 -- Tabela de Agendamentos:
 	-- id_agendamento: vai ser a  PK, identificando cada agendamento na 
@@ -72,7 +50,44 @@ CREATE TABLE servico (
 	-- data: data marcada para a realização do serviço, não podendo ser vazio.
 	-- hora: horário agendado para o início do atendimento, não podendo ser vazio.
 	-- observacao: comentários adicionais sobre o serviço e/ou observações do veículo.
-	-- finalizado: indica se o serviço já foi concluído, não podendo ser vazio..
+	-- finalizado: indica se o serviço já foi concluído, não podendo ser vazio.
+
+-- Relações de Cardinalidade: 
+	-- cliente 1:N veiculo: Um cliente pode possuir vários veículos, mas cada 
+	-- veículo pertence a um único cliente.
+	-- cliente 1:N agendamento: Um cliente pode realizar vários agendamentos, mas 
+	-- cada agendamento pertence a um único cliente.
+	-- veiculo 1:N agendamento: Um veículo pode ter vários agendamentos, mas cada 
+	-- agendamento está associado a um único veículo.
+	-- servico 1:N agendamento: Um serviço pode ser executado em vários agendamentos, mas cada agendamento se refere a um único serviço.
+
+CREATE TABLE cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cpf CHAR(11) UNIQUE NOT NULL,
+    numero_telefone VARCHAR(20) NOT NULL,
+    endereco CHAR(100) NOT NULL
+);
+
+CREATE TABLE veiculo (
+    id_veiculo INT AUTO_INCREMENT PRIMARY KEY,
+    id_cliente_ref INT NOT NULL,
+    placa CHAR(7) UNIQUE NOT NULL,
+    renavam VARCHAR(11) UNIQUE NOT NULL,
+    marca VARCHAR(40) NOT NULL,
+    modelo VARCHAR(60) NOT NULL,
+    cor VARCHAR(30) NOT NULL,
+    ano_fabricacao DATE NOT NULL
+);
+
+CREATE TABLE servico (
+    id_servico INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(80) UNIQUE NOT NULL,
+    descricao VARCHAR(255),
+    duracao TIME,
+    preco DOUBLE NOT NULL CHECK (preco > 0)
+);
+
 CREATE TABLE agendamento (
     id_agendamento INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente_ref INT NOT NULL,
@@ -83,12 +98,3 @@ CREATE TABLE agendamento (
     observacao VARCHAR(255),
     finalizado BOOLEAN NOT NULL
 );
-
--- Relações de Cardinalidade: 
--- cliente 1:N veiculo: Um cliente pode possuir vários veículos, mas cada 
--- veículo pertence a um único cliente.
--- cliente 1:N agendamento: Um cliente pode realizar vários agendamentos, mas 
--- cada agendamento pertence a um único cliente.
--- veiculo 1:N agendamento: Um veículo pode ter vários agendamentos, mas cada 
--- agendamento está associado a um único veículo.
--- servico 1:N agendamento: Um serviço pode ser executado em vários agendamentos, mas cada agendamento se refere a um único serviço.
